f5c5718ff81020b537cb48a84bffe0a145b706e6
rupangi-mehta <rupangi.mehta.25@gmail.com>
2013-09-28 03:06:09 -0400

    main activity and buttons.
    created main activity and added functionality to buttons.

---
+++ src/com/game/forcesofnature/HelpGame.java
@@ -0,0 +1,17 @@
+package com.game.forcesofnature;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HelpGame extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.help_game);
+	}
+	@Override
+	public void setContentView(View view) {
+		super.setContentView(view);
+	}
+}
--- src/com/game/forcesofnature/MainActivity.java
+++ src/com/game/forcesofnature/MainActivity.java
@@ -1,8 +1,13 @@
 package com.game.forcesofnature;
 
-import android.os.Bundle;
+import com.game.forcesofnature.playgame.PlayGame;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setListeners();
     }
 
 
@@ -19,5 +25,26 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    
+
+	private void setListeners() {
+		Button playButton= (Button) findViewById(R.id.playButton);
+		playButton.setOnClickListener(new View.OnClickListener() {
+		    @Override
+		    public void onClick(View v) {
+		    	Intent intent = new Intent(MainActivity.this, PlayGame.class);
+		    	startActivity(intent);
+		    }
+		});
+		
+		Button helpButton= (Button) findViewById(R.id.howToPlayButton);
+	    helpButton.setOnClickListener(new View.OnClickListener() {
+		    @Override
+		    public void onClick(View v) {
+		    	Intent intent = new Intent(MainActivity.this, HelpGame.class);
+		    	startActivity(intent);
+		    }  
+	    });
+	}
     
 }
